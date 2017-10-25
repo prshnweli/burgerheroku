8aa583099d2b5703ce2a179c61c558f9e6032296
Prashan Welipitiya <prshn.weli@gmail.com>
2017-10-24 21:59:32 -0700

    added sql stuff

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured, _date) VALUES ("double-double", false, '2017-10-24');
+INSERT INTO burgers (burger_name, devoured, _date) VALUES ("ham", false, '2017-10-14');
+INSERT INTO burgers (burger_name, devoured, _date) VALUES ("cheese", false, '2017-10-20');
